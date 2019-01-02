13de00825968f040c91bd061c8123f2398eb9574
Brian Peck <brian.peck@thecontrolgroup.com>
2019-01-01 22:48:17 -0800

    Added ARP and Ethernet Packet and combined them

--- network_scanner.py
+++ network_scanner.py
@@ -3,6 +3,14 @@
 import scapy.all as scapy
 
 def scan(ip):
-	scapy.arping(ip)
+	arp_request = scapy.ARP(pdst=ip) # contains an instance of an ARP Packet with pdst(IPField) set to ip
+	arp_request.show() # Shows details of create ARP instance
+	# print(arp_request.summary()) # scapy method gives summary of packet created above
+	# scapy.ls(scapy.ARP()) # gives options for specified class scapy.ARP()
+	broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff') # Creates an ethernet opject instance
+	broadcast.show()
+	arp_request_broadcast = broadcast/arp_request # Combines broadcast and arp_requet packet using Scapy
+	# print(arp_request_broadcast.summary())
+	arp_request_broadcast.show()
 
-scan("172.20.10.1/24")
+scan("192.168.1.1/24")

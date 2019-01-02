39aed8e9497c366adbd02a0547e58ecf18cd8643
Brian Peck <brian.peck@thecontrolgroup.com>
2019-01-01 23:12:53 -0800

    Removed the .show() functions and added scapy send recieve custom method and stored it in answered/unanswered

--- network_scanner.py
+++ network_scanner.py
@@ -4,13 +4,20 @@ import scapy.all as scapy
 
 def scan(ip):
 	arp_request = scapy.ARP(pdst=ip) # contains an instance of an ARP Packet with pdst(IPField) set to ip
-	arp_request.show() # Shows details of create ARP instance
+
+	# arp_request.show() # Shows details of create ARP instance
+
 	# print(arp_request.summary()) # scapy method gives summary of packet created above
+
 	# scapy.ls(scapy.ARP()) # gives options for specified class scapy.ARP()
-	broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff') # Creates an ethernet opject instance
-	broadcast.show()
+
+	broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff') # Creates an ethernet opject instance... default sends packet to broadcast MAC address
+
 	arp_request_broadcast = broadcast/arp_request # Combines broadcast and arp_requet packet using Scapy
-	# print(arp_request_broadcast.summary())
-	arp_request_broadcast.show()
+
+	answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1) # scapy send recieve custom method to send the packets, captured in answered and unanswered variables
+
+	print answered.summary()
+
 
 scan("192.168.1.1/24")
